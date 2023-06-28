cefa16be2dda837ce2dda4ad4cabe847b0505a72
Francis Targanski <francis@targanski.com>
2023-06-28 14:52:39 -0300

    Feature (Slider) addin slider and API config

--- components/main/content.tsx
+++ components/main/content.tsx
@@ -1,69 +1,121 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./main.module.css";
 import data from "../../server/lessons.json";
-import { getTexts } from "../../utils/textUtils";
 import Image from "next/image";
-import IconBook from "../../public/images/icon-book.svg"
-import IconVideo from "../../public/images/icon-video.svg"
-import IconTest from "../../public/images/icon-tests.svg"
-import IconTask from "../../public/images/icon-tasks.svg"
+import IconBook from "../../public/images/icon-book.svg";
+import IconVideo from "../../public/images/icon-video.svg";
+import IconTest from "../../public/images/icon-tests.svg";
+import IconTask from "../../public/images/icon-tasks.svg";
+import { Spotlight } from "../../types/types";
+import axios from "axios";
+import { getTexts } from "../../utils/textUtils";
 
 const Content = () => {
   const t = getTexts();
+  const [courseData, setCourseData] = useState<Spotlight[] | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.beta.unycos.com/u/courses/spotlights/natacion",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "x-mejor-key": "unycos",
+            },
+          }
+        );
+        setCourseData(response.data.spotlights);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <><section className={styles.exclusive}>
-      <h3 className={styles.title}>{t.content.exclusiveTitle}</h3>
-      <div className={styles.exclusiveIcons}>
+    <>
+      <section className={styles.slider}>
+        <div className={styles.sliderContainer}>
+          {courseData && courseData.length > 0 && (
+            <div className={styles.sliderItem}>
+              <div
+                className={styles.sliderItemImage}
+                style={{ backgroundImage: `url(${courseData[0].imageUrl})` }}
+              />
 
-        <div className={styles.icons}>
-        <Image
-            className={styles.icon}
-            src={IconBook}
-            alt={t.content.altIconBook}
-          />
-          <p className={styles.iconText}>{t.content.iconBook} </p>
+              <div className={styles.sliderCourses}>
+                <div className={styles.sliderCourseMain}>
+                  <div className={styles.sliderCourseMainTitle}>
+                    {courseData[0].title}
+                  </div>
+                  <div className={styles.sliderCourseMainResume}>
+                    {courseData[0].description}
+                  </div>
+                </div>
+
+                <div className={styles.sliderCourseOthers}>
+                  {courseData.slice(1).map((spotlight: Spotlight) => (
+                    <div key={spotlight.title}>
+                      <div className={styles.sliderCourseTitle}>
+                        {spotlight.title}
+                      </div>
+                      <div
+                        className={styles.sliderCourseImage}
+                        style={{ backgroundImage: `url(${spotlight.image})` }}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
+      </section>
 
-        <div className={styles.icons}>
-        <Image
-            className={styles.icon}
-            src={IconVideo}
-            alt={t.content.altIconVideo}
-          />
-          <p className={styles.iconText}>{t.content.iconVideo} </p>
+      <section className={styles.exclusive}>
+        <h3 className={styles.title}>{t.content.exclusiveTitle}</h3>
+        <div className={styles.exclusiveIcons}>
+          <div className={styles.icons}>
+            <Image
+              className={styles.icon}
+              src={IconBook}
+              alt={t.content.altIconBook}
+            />
+            <p className={styles.iconText}>{t.content.iconBook} </p>
+          </div>
+          <div className={styles.icons}>
+            <Image
+              className={styles.icon}
+              src={IconVideo}
+              alt={t.content.altIconVideo}
+            />
+            <p className={styles.iconText}>{t.content.iconVideo} </p>
+          </div>
+          <div className={styles.icons}>
+            <Image
+              className={styles.icon}
+              src={IconTest}
+              alt={t.content.altIconTest}
+            />
+            <p className={styles.iconText}>{t.content.iconTest} </p>
+          </div>
+          <div className={styles.icons}>
+            <Image
+              className={styles.icon}
+              src={IconTask}
+              alt={t.content.altIconTask}
+            />
+            <p className={styles.iconText}>{t.content.iconTask} </p>
+          </div>
         </div>
-
-        <div className={styles.icons}>
-        <Image
-            className={styles.icon}
-            src={IconTest}
-            alt={t.content.altIconTest}
-          />
-          <p className={styles.iconText}>{t.content.iconTest} </p>
-        </div>
-
-        <div className={styles.icons}>
-        <Image
-            className={styles.icon}
-            src={IconTask}
-            alt={t.content.altIconTask}
-          />
-          <p className={styles.iconText}>{t.content.iconTask} </p>
-        </div>
-
-
-
-        
-      </div>
-      <button className={styles.exclusiveButton}>
-            <p className={styles.exclusiveButtonText}>
-              {t.content.exclusiveButtonText}
-            </p>
-          </button>
-
-    </section>
-
-
+        <button className={styles.exclusiveButton}>
+          <p className={styles.exclusiveButtonText}>
+            {t.content.exclusiveButtonText}
+          </p>
+        </button>
+      </section>
 
       <section className={styles.lessons}>
         <h3 className={styles.title}>{t.content.lessonsTittle}</h3>
--- types/types.ts
+++ types/types.ts
@@ -1,5 +1,12 @@
 export interface Country {
-    name: string;
-    country_code: string;
-    phone: string;
-  };
+  name: string;
+  country_code: string;
+  phone: string;
+};
+
+export interface Spotlight {
+  image: any;
+  title: string;
+  description: string;
+  imageUrl: string;
+}
