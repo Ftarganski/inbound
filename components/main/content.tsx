78902fcb9ab0e58d5b4682912d62c53cb657ead5
Francis Targanski <francis@targanski.com>
2023-06-28 12:09:23 -0300

    Feature (Content) adding layout and lessons json to consume on html

--- components/main/content.tsx
+++ components/main/content.tsx
@@ -1,11 +1,91 @@
 import React from "react";
-import styles from './main.module.css'
+import styles from "./main.module.css";
+import data from "../../server/lessons.json";
+import { getTexts } from "../../utils/textUtils";
+import Image from "next/image";
+import IconBook from "../../public/images/icon-book.svg"
+import IconVideo from "../../public/images/icon-video.svg"
+import IconTest from "../../public/images/icon-tests.svg"
+import IconTask from "../../public/images/icon-tasks.svg"
 
 const Content = () => {
-
+  const t = getTexts();
   return (
-    <>
-      
+    <><section className={styles.exclusive}>
+      <h3 className={styles.title}>{t.content.exclusiveTitle}</h3>
+      <div className={styles.exclusiveIcons}>
+
+        <div className={styles.icons}>
+        <Image
+            className={styles.icon}
+            src={IconBook}
+            alt={t.content.altIconBook}
+          />
+          <p className={styles.iconText}>{t.content.iconBook} </p>
+        </div>
+
+        <div className={styles.icons}>
+        <Image
+            className={styles.icon}
+            src={IconVideo}
+            alt={t.content.altIconVideo}
+          />
+          <p className={styles.iconText}>{t.content.iconVideo} </p>
+        </div>
+
+        <div className={styles.icons}>
+        <Image
+            className={styles.icon}
+            src={IconTest}
+            alt={t.content.altIconTest}
+          />
+          <p className={styles.iconText}>{t.content.iconTest} </p>
+        </div>
+
+        <div className={styles.icons}>
+        <Image
+            className={styles.icon}
+            src={IconTask}
+            alt={t.content.altIconTask}
+          />
+          <p className={styles.iconText}>{t.content.iconTask} </p>
+        </div>
+
+
+
+        
+      </div>
+      <button className={styles.exclusiveButton}>
+            <p className={styles.exclusiveButtonText}>
+              {t.content.exclusiveButtonText}
+            </p>
+          </button>
+
+    </section>
+
+
+
+      <section className={styles.lessons}>
+        <h3 className={styles.title}>{t.content.lessonsTittle}</h3>
+        <div className={styles.gridContainer}>
+          {data.slice(0, 5).map((item) => (
+            <div key={item.id} className={styles.gridItem}>
+              <p className={styles.lessonID}>
+                {item.id.toString().padStart(2, "0")}
+              </p>
+              <div>
+                <h4 className={styles.lessonTitle}>{item.title}</h4>
+                <p className={styles.lessonResume}>{item.resume}</p>
+              </div>
+            </div>
+          ))}
+          <button className={styles.lessonButton}>
+            <p className={styles.lessonButtonText}>
+              {t.content.lessonsButtonText}
+            </p>
+          </button>
+        </div>
+      </section>
     </>
   );
 };
