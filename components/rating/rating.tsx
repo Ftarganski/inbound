895a1bf4ea62b20deac0f5f946869e72f76396cb
Francis Targanski <francis@targanski.com>
2023-06-28 23:02:52 -0300

    Feature (final desktop) final desktop version

--- components/rating/rating.tsx
+++ components/rating/rating.tsx
@@ -3,10 +3,19 @@ import styles from "./rating.module.css";
 import data from "../../server/rating.json";
 import Image from "next/image";
 import ThumbsUp from "../../public/images/thumbs-up.svg";
+import ThumbsUpGrey from "../../public/images/thumbs-up-grey.svg";
+import Checkmark from"../../public/images/checkmark.svg";
 import { getTexts } from "../../utils/textUtils";
 
 const Rating = () => {
   const t = getTexts();
+
+  const getInitials = (name:any) => {
+    const names = name.split(" ");
+    let initials = names.map((n:any) => n[0]);
+    initials = initials.slice(0, 2);
+    return initials.join("").toUpperCase();
+  };
 
   return (
     <>
@@ -25,21 +34,34 @@ const Rating = () => {
         </div>
 
         <div className={styles.gridContainer}>
-        {data.slice(0, 3).map((item, index) => (
-            <div key={item.id} className={`${styles.gridItem} ${
+          {data.slice(0, 3).map((item, index) => (
+            <div
+              key={item.id}
+              className={`${styles.gridItem} ${
                 index < 2 ? styles.hasBorder : ""
-              }`}>
-              <Image
-                className={styles.logo}
-                // src={item.imageUrl}
-                src="/public/images/icon-facebook.svg"
-                alt={t.rating.altImage}
-                width={80}
-                height={80}
-              />
+              }`}
+            >
+              {item.imageUrl ? (
+                <Image
+                className={styles.photoUser}
+                  src={item.imageUrl}
+                  alt={t.rating.altImage}
+                  width={80}
+                  height={80}
+                />
+              ) : (
+                <div className={styles.initials}>
+                  <p>{getInitials(item.name)}</p>
+                  </div>
+              )}
               <div>
                 <div className={styles.testUser}>
                   <div className={styles.name}>{item.name}</div>
+                    <Image
+                      className={styles.thumbsRating}
+                      src={ThumbsUpGrey}
+                      alt={t.rating.altThumbs}
+                    />
                   <div className={styles.date}>{item.date}</div>
                 </div>
 
@@ -53,6 +75,13 @@ const Rating = () => {
                   {item.response}
                 </div>
                 <div className={styles.equip}>
+                {item.response && (
+                    <Image
+                      className={styles.checkmark}
+                      src={Checkmark}
+                      alt={t.rating.altCheckmark}
+                    />
+                  )}               
                   <div className={styles.user}>{item.user}</div>
                   <div className={styles.userRole}>{item.userRole}</div>
                   <div className={styles.responseDate}>{item.responseDate}</div>
