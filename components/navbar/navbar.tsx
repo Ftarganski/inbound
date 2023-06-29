1c6d51fcff9f0f1f66658d9103c41b9d3fdc99f4
Francis Targanski <francis@targanski.com>
2023-06-28 21:46:13 -0300

    Feature (Navbar) add fake login to show menu

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Logo from "../../public/images/logo-unycos.svg";
+import Arrow from "../../public/images/chevron-down.svg"
 import { getTexts } from "../../utils/textUtils";
 
 const Navbar = () => {
   const t = getTexts();
+
+  const [isLogged, setIsLogged] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [showLogin, setShowLogin] = useState(false); // Novo estado para exibir o login
+
+  const handleLoginClick = () => {
+    setIsLogged(true);
+  };
+
+  const handleDropdownClick = () => {
+    setShowDropdown(!showDropdown);
+    setShowLogin(false); // Oculta o login ao exibir o dropdown
+  };
 
   return (
     <>
@@ -23,14 +37,54 @@ const Navbar = () => {
 
           <div className={styles.navbarItem}>
             <ul>
-              <li>{t.navbar.courses}</li>
-              <li>{t.navbar.register}</li>
-              <li>{t.navbar.login}</li>
-            </ul>
+              <li className={styles.navbarLink}>{t.navbar.courses}</li>
+              {!isLogged && <li className={styles.navbarLink}>{t.navbar.register}</li>}
 
+              {!isLogged && (
+                <li className={styles.navbarLink} onClick={handleLoginClick}>{t.navbar.login}</li>
+              )}
+
+              {isLogged && (
+                <li className={styles.navbarLinkDrop} onClick={handleDropdownClick}>
+                  {showLogin ? (
+                    t.navbar.login
+                  ) : (
+                    <>
+                      <div>
+                      {t.navbar.hello}
+                      {t.navbar.user}
+                      <Image
+                        className={styles.arrow}
+                        src={Arrow}
+                        alt={t.navbar.altArrow}
+                      />
+                      </div>
+                      {showDropdown && (
+                        <ul className={styles.dropdown}>
+                          <li className={styles.dropdownLink}>{t.navbar.lkcontrolpanel}</li>
+                          <li className={styles.dropdownLink}>{t.navbar.lkcourse}</li>
+                          <li className={styles.dropdownLink}>{t.navbar.lkprofile}</li>
+                          <li className={styles.dropdownLink}>{t.navbar.lkaccount}</li>
+                          <li
+                            className={styles.dropdownLink}
+                            onClick={() => {
+                              setIsLogged(false);
+                              setShowLogin(true); // Exibe o login ao fazer logout
+                            }}
+                          >
+                            {t.navbar.lklogout}
+                          </li>
+                        </ul>
+                      )}
+                    </>
+                  )}
+                </li>
+              )}
+            </ul>
           </div>
         </div>
       </section>
+      
     </>
   );
 };
