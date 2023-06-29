ded4a81448905281b2307969a12d2735b0dbb4e8
Francis Targanski <francis@targanski.com>
2023-06-29 20:04:11 -0300

    Feature (Lessons) adjust mobile query

--- components/lessons/lessons.tsx
+++ components/lessons/lessons.tsx
@@ -6,19 +6,27 @@ import IconBook from "../../public/images/icon-book.svg";
 import IconVideo from "../../public/images/icon-video.svg";
 import IconTest from "../../public/images/icon-tests.svg";
 import IconTask from "../../public/images/icon-tasks.svg";
+import ArrowR from "../../public/images/chevron-right.svg";
 import { Spotlight } from "../../types/types";
 import { getTexts } from "../../utils/textUtils";
+import { useWindowResize } from "../../hooks/useWindowResize";
 
 const Lessons = () => {
   const t = getTexts();
+  const isMobile = useWindowResize();
   const [courseData, setCourseData] = useState<Spotlight[] | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
-
   return (
     <>
-           <section className={styles.exclusive}>
-        <h3 className={styles.title}>{t.content.exclusiveTitle}</h3>
+      <section className={styles.exclusive}>
+        {isMobile ? (
+          <></>
+        ) : (
+          <>
+            <h3 className={styles.title}>{t.content.lessonsTittle}</h3>
+          </>
+        )}
         <div className={styles.exclusiveIcons}>
           <div className={styles.icons}>
             <Image
@@ -53,33 +61,85 @@ const Lessons = () => {
             <p className={styles.iconText}>{t.content.iconTask} </p>
           </div>
         </div>
-        <button className={styles.exclusiveButton}>
-          <p className={styles.exclusiveButtonText}>
-            {t.content.exclusiveButtonText}
-          </p>
-        </button>
+
+        {isMobile ? (
+          <></>
+        ) : (
+          <>
+            <button className={styles.exclusiveButton}>
+              <p className={styles.exclusiveButtonText}>
+                {t.content.exclusiveButtonText}
+              </p>
+            </button>
+          </>
+        )}
       </section>
 
       <section className={styles.lessons}>
-        <h3 className={styles.title}>{t.content.lessonsTittle}</h3>
-        <div className={styles.gridContainer}>
-          {data.slice(0, 5).map((item) => (
-            <div key={item.id} className={styles.gridItem}>
-              <p className={styles.lessonID}>
-                {item.id.toString().padStart(2, "0")}
-              </p>
-              <div>
-                <h4 className={styles.lessonTitle}>{item.title}</h4>
-                <p className={styles.lessonResume}>{item.resume}</p>
-              </div>
+        {isMobile ? (
+          <>
+            <h3 className={styles.title}>{t.content.lessonsHighlights}</h3>
+          </>
+        ) : (
+          <>
+            <h3 className={styles.title}>{t.content.lessonsTittle}</h3>
+          </>
+        )}
+
+        {isMobile ? (
+          <>
+            <div className={styles.gridContainer}>
+              {data.slice(0, 4).map((item, index) => (
+                <div
+                  key={item.id}
+                  className={`${styles.gridItem} ${
+                    index === 3 ? styles.lastItem : ""
+                  }`}
+                >
+                  <p className={styles.lessonID}>
+                    {item.id.toString().padStart(2, "0")}
+                  </p>
+                  <h4 className={styles.lessonTitle}>{item.title}</h4>
+
+                  <Image
+              className={styles.arrowR}
+              src={ArrowR}
+              alt={t.content.altArrowR}
+            />
+
+
+
+                </div>
+              ))}
+              <button className={styles.highlightsButton}>
+                <p className={styles.highlightsButtonText}>
+                  {t.content.highlightssButtonText}
+                </p>
+              </button>
             </div>
-          ))}
-          <button className={styles.lessonButton}>
-            <p className={styles.lessonButtonText}>
-              {t.content.lessonsButtonText}
-            </p>
-          </button>
-        </div>
+          </>
+        ) : (
+          <>
+            <div className={styles.gridContainer}>
+              {data.slice(0, 5).map((item) => (
+                <div key={item.id} className={styles.gridItem}>
+                  <p className={styles.lessonID}>
+                    {item.id.toString().padStart(2, "0")}
+                  </p>
+                  <div>
+                    <h4 className={styles.lessonTitle}>{item.title}</h4>
+                    <p className={styles.lessonResume}>{item.resume}</p>
+                  </div>
+                </div>
+              ))}
+              <button className={styles.lessonButton}>
+                <p className={styles.lessonButtonText}>
+                  {t.content.lessonsButtonText}
+                </p>
+              </button>
+            </div>
+          </>
+        )}
       </section>
     </>
   );
