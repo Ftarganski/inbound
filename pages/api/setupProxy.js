0b0f32b9ad832b8f89ce4cb6377276720f0a27e6
Francis Targanski <francis@targanski.com>
2023-06-29 22:07:56 -0300

    Feature (Proxy) trying fix CORS error

--- pages/api/setupProxy.js
+++ pages/api/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://api.beta.unycos.com/u/courses/spotlights/natacion',
+      target: 'https://api.beta.unycos.com',
       changeOrigin: true,
       headers: {
         'Content-Type': 'application/json',
