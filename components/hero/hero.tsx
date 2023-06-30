7bd222574d237d611d794badbb38488dfbc2ed19
Francis Targanski <francis@targanski.com>
2023-06-29 23:55:41 -0300

    Feature (menu mobile) adding menu mobile

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -14,88 +14,85 @@ const Hero = () => {
   return (
     <>
       <section className={styles.hero}>
-
-
         {isMobile ? (
           <>
-           {data
-          .filter((item) => item.id === 5)
-          .map((item) => (
-            <div
-              key={item.id}
-              className={styles.heroImage}
-              style={{
-                backgroundImage: `radial-gradient(82% 65% at 50% 50%, rgba(0, 0, 0, 0.00) 56%, #000 100%), url(${item.imageUrl})`,
-              }}
-            >
-              <h2 className={styles.heroTitle}>{item.speaker}</h2>
-              <p
-                className={styles.heroSubtitle}
-                dangerouslySetInnerHTML={{ __html: item.title }}
-              />
+            {data
+              .filter((item) => item.id === 5)
+              .map((item) => (
+                <div
+                  key={item.id}
+                  className={styles.heroImage}
+                  style={{
+                    backgroundImage: `radial-gradient(82% 65% at 50% 50%, rgba(0, 0, 0, 0.00) 56%, #000 100%), url(${item.imageUrl})`,
+                  }}
+                >
+                  <h2 className={styles.heroTitle}>{item.speaker}</h2>
+                  <p
+                    className={styles.heroSubtitle}
+                    dangerouslySetInnerHTML={{ __html: item.title }}
+                  />
+                </div>
+              ))}
+
+            <button className={styles.heroButton}>
+              <p className={styles.heroButtonText}>{t.hero.ButtonTextInfo}</p>
+            </button>
+
+            <div className={styles.heroData}>
+              <div className={styles.heroInfo}>
+                <div className={styles.heroBorder}>
+                  <Image
+                    className={styles.heroIcon}
+                    src={IconBook}
+                    alt={t.hero.altIconBook}
+                  />
+                </div>
+                <p className={styles.heroIconText}>{t.hero.iconBook}</p>
+              </div>
+
+              <div className={styles.heroInfo}>
+                <div className={styles.heroBorder}>
+                  <Image
+                    className={styles.heroIcon}
+                    src={IconFilm}
+                    alt={t.hero.altIconFilm}
+                  />
+                </div>
+                <p className={styles.heroIconText}>{t.hero.iconFilm}</p>
+              </div>
             </div>
-          ))}
-          
-          <button className={styles.heroButton}>
-            <p className={styles.heroButtonText}>{t.hero.ButtonTextInfo}</p>
-          </button>
 
-          <div className={styles.heroData}>
-          <div className={styles.heroInfo}>
-            <div className={styles.heroBorder}>
-              <Image
-                className={styles.heroIcon}
-                src={IconBook}
-                alt={t.hero.altIconBook}
-              />
-            </div>
-            <p className={styles.heroIconText}>{t.hero.iconBook}</p>
-          </div>
+            <h3 className={styles.heroCitation}>{t.hero.citation}</h3>
 
-          <div className={styles.heroInfo}>
-            <div className={styles.heroBorder}>
-              <Image
-                className={styles.heroIcon}
-                src={IconFilm}
-                alt={t.hero.altIconFilm}
-              />
-            </div>
-            <p className={styles.heroIconText}>{t.hero.iconFilm}</p>
-          </div>
+            <button className={styles.ButtonQuestion}>
+              <p className={styles.ButtonTextQuestion}>
+                {t.hero.ButtonTextQuestion}
+              </p>
+            </button>
 
-          
-        </div>
+            <h3 className={styles.heroCitation}>{t.hero.goal}</h3>
 
-        <h3 className={styles.heroCitation}>{t.hero.citation}</h3>
-             
-        <button className={styles.ButtonQuestion}>
-            <p className={styles.ButtonTextQuestion}>{t.hero.ButtonTextQuestion}</p>
-          </button>
-
-          <h3 className={styles.heroCitation}>{t.hero.goal}</h3>
-        
-        <p className={styles.heroInspiration}>{t.hero.inspiration}</p>
-          
+            <p className={styles.heroInspiration}>{t.hero.inspiration}</p>
           </>
         ) : (
           <>
- {data
-          .filter((item) => item.id === 4)
-          .map((item) => (
-            <div
-              key={item.id}
-              className={styles.heroImage}
-              style={{
-                backgroundImage: `radial-gradient(82% 65% at 50% 50%, rgba(0, 0, 0, 0.00) 56%, #000 100%), url(${item.imageUrl})`,
-              }}
-            >
-              <h2 className={styles.heroTitle}>{item.speaker}</h2>
-              <p
-                className={styles.heroSubtitle}
-                dangerouslySetInnerHTML={{ __html: item.title }}
-              />
-            </div>
-          ))}
+            {data
+              .filter((item) => item.id === 4)
+              .map((item) => (
+                <div
+                  key={item.id}
+                  className={styles.heroImage}
+                  style={{
+                    backgroundImage: `radial-gradient(82% 65% at 50% 50%, rgba(0, 0, 0, 0.00) 56%, #000 100%), url(${item.imageUrl})`,
+                  }}
+                >
+                  <h2 className={styles.heroTitle}>{item.speaker}</h2>
+                  <p
+                    className={styles.heroSubtitle}
+                    dangerouslySetInnerHTML={{ __html: item.title }}
+                  />
+                </div>
+              ))}
 
             <div className={styles.heroData}>
               <div className={styles.heroInfo}>
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Logo from "../../public/images/logo-unycos.svg";
-import Arrow from "../../public/images/chevron-down.svg"
+import LogoBlack from "../../public/images/logo-unycos-black.svg";
+import Arrow from "../../public/images/chevron-down.svg";
+import Menu from "../../public/images/bx-menu.svg";
+import Whatsapp from "../../public/images/bxl-whatsapp.svg";
+import Notification from "../../public/images/bell-outline-on.svg";
 import { getTexts } from "../../utils/textUtils";
+import { useWindowResize } from "../../hooks/useWindowResize";
 
 const Navbar = () => {
   const t = getTexts();
-
+  const isMobile = useWindowResize();
   const [isLogged, setIsLogged] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
   const [showLogin, setShowLogin] = useState(false); // Novo estado para exibir o login
+
+  const handleMenuClick = () => {
+    setShowDropdown(!showDropdown);
+  };
 
   const handleLoginClick = () => {
     setIsLogged(true);
@@ -35,56 +44,148 @@ const Navbar = () => {
             />
           </div>
 
-          <div className={styles.navbarItem}>
-            <ul>
-              <li className={styles.navbarLink}>{t.navbar.courses}</li>
-              {!isLogged && <li className={styles.navbarLink}>{t.navbar.register}</li>}
+          {isMobile ? (
+            <>
+              <div className={styles.navbarItem}>
+                <Image
+                  className={styles.menu}
+                  src={Menu}
+                  alt={t.navbar.altMenu}
+                  onClick={handleMenuClick}
+                />
+              </div>
 
-              {!isLogged && (
-                <li className={styles.navbarLink} onClick={handleLoginClick}>{t.navbar.login}</li>
-              )}
-
-              {isLogged && (
-                <li className={styles.navbarLinkDrop} onClick={handleDropdownClick}>
-                  {showLogin ? (
-                    t.navbar.login
-                  ) : (
-                    <>
-                      <div>
+              {showDropdown && (
+                <div className={styles.dropdownMobile}>
+                  <div className={styles.stripe}></div>
+                  <ul>
+                    <li>
+                      <Image
+                        className={styles.logoBlack}
+                        src={LogoBlack}
+                        alt={t.navbar.altLogoUnycos}
+                      />
+                    </li>
+                    <li className={styles.dropdownLinkUser}>
                       {t.navbar.hello}
                       {t.navbar.user}
                       <Image
-                        className={styles.arrow}
-                        src={Arrow}
-                        alt={t.navbar.altArrow}
+                        className={styles.iconNotification}
+                        src={Notification}
+                        alt={t.navbar.altNotification}
                       />
-                      </div>
-                      {showDropdown && (
-                        <ul className={styles.dropdown}>
-                          <li className={styles.dropdownLink}>{t.navbar.lkcontrolpanel}</li>
-                          <li className={styles.dropdownLink}>{t.navbar.lkcourse}</li>
-                          <li className={styles.dropdownLink}>{t.navbar.lkprofile}</li>
-                          <li className={styles.dropdownLink}>{t.navbar.lkaccount}</li>
-                          <li
-                            className={styles.dropdownLink}
-                            onClick={() => {
-                              setIsLogged(false);
-                              setShowLogin(true); // Exibe o login ao fazer logout
-                            }}
-                          >
-                            {t.navbar.lklogout}
-                          </li>
-                        </ul>
-                      )}
-                    </>
-                  )}
-                </li>
+                    </li>
+                    <li className={styles.dropdownLink}>
+                      {t.navbar.lkcontrolpanel}
+                    </li>
+                    <li className={styles.dropdownLink}>{t.navbar.lkcourse}</li>
+                    <li className={styles.dropdownLink}>
+                      {t.navbar.lkprofile}
+                    </li>
+                    <li className={styles.dropdownLink}>
+                      {t.navbar.lkaccount}
+                    </li>
+                    <li className={styles.dropdownLink}>
+                      {t.navbar.lkallaccount}
+                    </li>
+                    <li className={styles.dropdownLink}>
+                      <p className={styles.dropdownSubtitle}>
+                        {t.navbar.subtitle}
+                      </p>
+                      <p className={styles.dropdownPhone}>
+                        <Image
+                          className={styles.iconWhats}
+                          src={Whatsapp}
+                          alt={t.navbar.altIconWhats}
+                        />
+
+                        {t.footer.contactButtonText}
+                      </p>
+                    </li>
+                    <li
+                      onClick={() => {
+                        setIsLogged(false);
+                        setShowLogin(true); // Exibe o login ao fazer logout
+                      }}
+                      className={styles.dropdownLink}
+                    >
+                      {t.navbar.lklogout}
+                    </li>
+                  </ul>
+                </div>
               )}
-            </ul>
-          </div>
+            </>
+          ) : (
+            <>
+              <div className={styles.navbarItem}>
+                <ul>
+                  <li className={styles.navbarLink}>{t.navbar.courses}</li>
+                  {!isLogged && (
+                    <li className={styles.navbarLink}>{t.navbar.register}</li>
+                  )}
+
+                  {!isLogged && (
+                    <li
+                      className={styles.navbarLink}
+                      onClick={handleLoginClick}
+                    >
+                      {t.navbar.login}
+                    </li>
+                  )}
+
+                  {isLogged && (
+                    <li
+                      className={styles.navbarLinkDrop}
+                      onClick={handleDropdownClick}
+                    >
+                      {showLogin ? (
+                        t.navbar.login
+                      ) : (
+                        <>
+                          <div>
+                            {t.navbar.hello}
+                            {t.navbar.user}
+                            <Image
+                              className={styles.arrow}
+                              src={Arrow}
+                              alt={t.navbar.altArrow}
+                            />
+                          </div>
+                          {showDropdown && (
+                            <ul className={styles.dropdown}>
+                              <li className={styles.dropdownLink}>
+                                {t.navbar.lkcontrolpanel}
+                              </li>
+                              <li className={styles.dropdownLink}>
+                                {t.navbar.lkcourse}
+                              </li>
+                              <li className={styles.dropdownLink}>
+                                {t.navbar.lkprofile}
+                              </li>
+                              <li className={styles.dropdownLink}>
+                                {t.navbar.lkaccount}
+                              </li>
+                              <li
+                                className={styles.dropdownLink}
+                                onClick={() => {
+                                  setIsLogged(false);
+                                  setShowLogin(true); // Exibe o login ao fazer logout
+                                }}
+                              >
+                                {t.navbar.lklogout}
+                              </li>
+                            </ul>
+                          )}
+                        </>
+                      )}
+                    </li>
+                  )}
+                </ul>
+              </div>
+            </>
+          )}
         </div>
       </section>
-      
     </>
   );
 };
